5a7f1b6c5038bc332a636a3ee6880c73be9c52bb
dmdylan <dylandanowski@gmail.com>
2020-10-22 20:09:38 -0400

    Why doesn't cinemachine allow easy camera zoom when it does everything else

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,22 +4,24 @@ using UnityEngine;
 using Cinemachine;
 using UnityEngine.InputSystem;
 using UnityEngine.Animations;
+using DG.Tweening;
 
 public class CameraMovement : MonoBehaviour
 {
     private CinemachineVirtualCamera playerCamera;
+    private CinemachineTransposer transposer;
 
     // Start is called before the first frame update
     void Start()
     {
         playerCamera = FindObjectOfType<CinemachineVirtualCamera>();
+        transposer = GetComponent<CinemachineTransposer>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //Change so it is taking a ray float distance to the follow target to gauge distance
+    //Check if transposer values can be changed to adjust zoom.
+    //possiby change the rotation of the look at object to rotate camera?
+    //Can change rotation bias to change angle camera is looking while moving
 
     private void OnMouseScroll(InputValue value)
     {
@@ -28,13 +30,16 @@ public class CameraMovement : MonoBehaviour
         if(input.y > 0)
         {
             //DOTWEEN from current camera position towards player position?
-            playerCamera.m_Lens.FieldOfView -= 10;
+            transposer.m_FollowOffset.z -= 1f;
+            Debug.Log(playerCamera.m_LookAt.position);
+
         }
         else if(input.y < 0)
         {
-            playerCamera.m_Lens.FieldOfView += 10;
+            //playerCamera.m_Lens.FieldOfView += 10;
+            Debug.Log("Back scroll");
         }
 
-        Debug.Log(input);
+        //Debug.Log(input);
     }
 }
