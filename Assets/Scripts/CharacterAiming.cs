280b94abee239ead7a327863801ea11ea71b4036
dmdylan <dylandanowski@gmail.com>
2020-10-24 20:27:35 -0400

    Finally like how the camera and movement works.

---
+++ Assets/Scripts/CharacterAiming.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using UnityEngine.InputSystem;
+using UnityEngine.Animations;
+using DG.Tweening;
+
+public class CharacterAiming : MonoBehaviour
+{
+    [SerializeField] private float turnSpeed = 15f;
+    Camera playerCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerCamera = Camera.main;
+    }
+
+    private void FixedUpdate()
+    {
+        float yawCamera = playerCamera.transform.rotation.eulerAngles.y;
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/CharacterLocomotion.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterLocomotion : MonoBehaviour
+{
+    private Animator animator;
+    private CharacterController characterController;
+    private Vector2 movement;
+    [SerializeField] private float moveSpeed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        animator = GetComponent<Animator>();
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+    private void OnMove(InputValue value)
+    {
+        movement = value.Get<Vector2>();     
+    }
+
+    //Move speed is the same regardles of the characters move direction
+    //Might make it slower if they are moving backwards as opposed to forwards
+    private void MoveCharacter()
+    {
+        //use input from new input system
+        var movementDirection = new Vector3(movement.x, 0, movement.y);
+        
+        //translate it into local coordinates instead of world
+        movementDirection = transform.TransformDirection(movementDirection);
+        
+        //multiply it by move speed and deltatime
+        movementDirection = movementDirection * moveSpeed * Time.deltaTime;
+
+        animator.SetFloat("InputX", Mathf.Round(movement.x));
+        animator.SetFloat("InputY", Mathf.Round(movement.y));
+        characterController.Move(movementDirection);
+    }
+}
