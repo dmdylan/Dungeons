cabfb3031d398c6be14ce096e560dd884d5893a6
dmdylan <dylandanowski@gmail.com>
2020-10-26 17:47:53 -0400

    Jumping roughly implemented and replicates properly

--- Assets/Scripts/CharacterLocomotion.cs
+++ Assets/Scripts/CharacterLocomotion.cs
@@ -9,7 +9,10 @@ public class CharacterLocomotion : NetworkBehaviour
     private Animator animator;
     private CharacterController characterController;
     private Vector2 movement;
-    [SerializeField] private float moveSpeed = 0;
+    private float velocityY; 
+    [SerializeField] private float moveSpeed = 0f;
+    [SerializeField] private float jumpHeight = 1f;
+    [SerializeField] private float gravityMultiplier = 1f;
 
 
     public override void OnStartLocalPlayer()
@@ -28,28 +31,54 @@ public class CharacterLocomotion : NetworkBehaviour
         MoveCharacter();
     }
 
-    private void OnMove(InputValue value)
-    {
-        movement = value.Get<Vector2>();     
-    }
-
     //Move speed is the same regardles of the characters move direction
     //Might make it slower if they are moving backwards as opposed to forwards
     private void MoveCharacter()
     {
         if (!isLocalPlayer) return;
 
+        //stop gravity pog
+        if(characterController.isGrounded && velocityY < 0)
+        {
+            velocityY = 0;
+        }
+
+        //Makes sure the character is brought back to the ground and character controller is grounded
+        velocityY += Time.fixedDeltaTime * Physics.gravity.y;
+
         //use input from new input system
-        var movementDirection = new Vector3(movement.x, 0, movement.y);
-        
+        var movementDirection = new Vector3(movement.x, velocityY, movement.y);
+
         //translate it into local coordinates instead of world
         movementDirection = transform.TransformDirection(movementDirection);
-        
-        //multiply it by move speed and deltatime
-        movementDirection = movementDirection * moveSpeed * Time.deltaTime;
 
+        //multiply it by move speed and deltatime
+        movementDirection = movementDirection * moveSpeed * Time.fixedDeltaTime;
+
+        //TODO: Take animation out of movement script?
         animator.SetFloat("InputX", Mathf.Round(movement.x));
         animator.SetFloat("InputY", Mathf.Round(movement.y));
+        
         characterController.Move(movementDirection);
     }
+
+    #region InputCommands
+    private void OnMove(InputValue value)
+    {
+        if (!isLocalPlayer) return;
+
+        movement = value.Get<Vector2>();     
+    }
+
+    private void OnJump()
+    {
+        if (!isLocalPlayer) return;
+
+        if (characterController.isGrounded)
+        {
+            float jumpVelocity = Mathf.Sqrt(Physics.gravity.y * jumpHeight * gravityMultiplier);
+            velocityY = jumpVelocity;
+        }
+    }
+    #endregion
 }
