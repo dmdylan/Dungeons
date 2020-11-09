7a29553e831a40f3d24eafd514025502d3a7a565
dmdylan <dylandanowski@gmail.com>
2020-11-08 21:51:20 -0500

    Implementing ability stuff

---
+++ Assets/Scripts/Ability Stuff/AbilityInfoSO.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+//TODO: Either this is abstract and then making a different scriptable object with functions, or put
+//that into their own classes.
+public abstract class AbilityInfoSO : ScriptableObject
+{
+    [SerializeField] private string abilityName = null;
+    [SerializeField] private float abilityDamage = 0f;
+    [SerializeField] private int abilityID = 0;
+    [SerializeField] private TextField toolTip = null;
+}
