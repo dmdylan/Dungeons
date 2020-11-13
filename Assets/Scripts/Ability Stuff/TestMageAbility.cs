40081f1fa81375e4cf4c3917ac747844f40fdbbe
dmdylan <dylandanowski@gmail.com>
2020-11-12 20:45:47 -0500

    Need to change how ability system works so it intergrates with multiplayer better.

--- Assets/Scripts/Ability Stuff/TestMageAbility.cs
+++ Assets/Scripts/Ability Stuff/TestMageAbility.cs
@@ -9,6 +9,9 @@ public class TestMageAbility : AbilityTemplateObject
 {
     public override IEnumerator Use()
     {
+        GameObject iceLanceObject = Instantiate(this.AbilityGameObject);
+        //Cannot spawn objects without an active server. Most likely has to be called from a network behavior.
+        NetworkServer.Spawn(iceLanceObject);
         Debug.Log("Ability Used!");
         yield return null;
     }
